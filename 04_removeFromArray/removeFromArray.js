793352241b9aba1b9987d5948c7c98c9dfa9040e
Ken <knetngyn@gmail.com>
2022-07-13 23:17:46 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...num) {
+  let newArray = [];
+  array.forEach((target) => {
+    if (!num.includes(target)) {
+      newArray.push(target);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line

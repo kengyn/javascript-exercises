9a9e849156617e993e1c362c7bed097980f98f31
Ken <knetngyn@gmail.com>
2022-07-13 20:52:02 -0500

    Finish second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let result = "";
+  if (num < 0 || num == null || num == undefined) {
+    return "ERROR";
+  } else {
+    for (let index = 0; index < num; index++) {
+      result = result.concat(string);
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line

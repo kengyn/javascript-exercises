0e9aca59f1593748221f7593b53113486dacb57f
Ken <knetngyn@gmail.com>
2022-07-14 13:21:25 -0500

    Revise answer to ex07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,19 +1,20 @@
 const ftoc = function (temp) {
-  let celcius = (temp - 32) * (5 / 9);
-  if (celcius % 1 !== 0) {
-    return Math.round(celcius * 10) / 10;
-  } else {
-    return celcius;
-  }
+  return Math.round((temp - 32) * (5 / 9) * 10) / 10;
+  // if (celcius % 1 !== 0) {
+  //   return ;
+  // } else {
+  //   return celcius;
+  // }
 };
 
 const ctof = function (temp) {
-  let far = temp * (9 / 5) + 32;
-  if (far % 1 !== 0) {
-    return Math.round(far * 10) / 10;
-  } else {
-    return far;
-  }
+  return Math.round((temp * (9 / 5) + 32) * 10) / 10;
+  // let far = ;
+  // if (far % 1 !== 0) {
+  //   ;
+  // } else {
+  //   return far;
+  // }
 };
 
 // Do not edit below this line

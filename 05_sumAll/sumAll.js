a5d60db634a2c55f9e1f950659d90b6e74f06b62
Ken <knetngyn@gmail.com>
2022-07-14 12:12:14 -0500

    Initial answer for exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function (first, second) {
+  let result = 0;
+  if (
+    first < 0 ||
+    second < 0 ||
+    typeof second == "string" ||
+    Array.isArray(second)
+  ) {
+    return "ERROR";
+  } else if (first > second) {
+    for (i = second; i <= first; i++) {
+      result += i;
+    }
+  } else if (second > first) {
+    for (i = first; i <= second; i++) {
+      result += i;
+    }
+  } else {
+    ("ERROR");
+  }
+  return result;
 };
 
 // Do not edit below this line

baeadd03d1f9db6bd0851e20d71493eda0dee0ce
Ken <knetngyn@gmail.com>
2022-07-14 13:19:05 -0500

    Initial answer to ex07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,23 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let celcius = (temp - 32) * (5 / 9);
+  if (celcius % 1 !== 0) {
+    return Math.round(celcius * 10) / 10;
+  } else {
+    return celcius;
+  }
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let far = temp * (9 / 5) + 32;
+  if (far % 1 !== 0) {
+    return Math.round(far * 10) / 10;
+  } else {
+    return far;
+  }
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };

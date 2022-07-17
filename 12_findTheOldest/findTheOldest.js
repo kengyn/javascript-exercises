379c5c1853f3f1b48bce82419eed82bebaf18824
Ken <knetngyn@gmail.com>
2022-07-17 14:50:00 -0500

    Answer for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.forEach((person) => {
+    if (!person.hasOwnProperty("yearOfDeath")) {
+      person.yearOfDeath = 2022;
+    }
+  });
+  return people.sort(
+    (a, b) => b.yearOfDeath - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth)
+  )[0];
 };
 
 // Do not edit below this line

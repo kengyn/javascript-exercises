05259137a08bdd2c6d774c9079a64470dc42f632
Ken <knetngyn@gmail.com>
2022-07-17 12:53:09 -0500

    Scuffed initial answer to palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let newString = string
+    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+    .replace(/\s+/g, "")
+    .toLowerCase()
+    .split("");
+  let reverseString = newString.slice().reverse();
+  let superString = reverseString.toString();
+  let ss2String = newString.toString();
+  if (ss2String == superString) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line

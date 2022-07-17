86cc4f9561fe2175292769e1a520bf6cf9ab6916
Ken <knetngyn@gmail.com>
2022-07-17 12:58:05 -0500

    Revised answer to Palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,6 @@
 const palindromes = function (string) {
-  let newString = string
-    .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "")
-    .replace(/\s+/g, "")
-    .toLowerCase()
-    .split("");
-  let reverseString = newString.slice().reverse();
-  let superString = reverseString.toString();
-  let ss2String = newString.toString();
-  if (ss2String == superString) {
-    return true;
-  } else {
-    return false;
-  }
+  newString = string.toLowerCase().replace(/[^a-z]/g, "");
+  return newString.split("").reverse().join("") == newString;
 };
 
 // Do not edit below this line
